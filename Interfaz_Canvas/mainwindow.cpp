71509b76e62f3cdc645e85aef2283c2c1969b884
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2022-05-02 08:49:58 -0600

    Función agregada: dibujar una línea recta entre 2 puntos

---
+++ Interfaz_Canvas/mainwindow.cpp
@@ -0,0 +1,70 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QMouseEvent"
+#include "QPixmap"
+#include "QPainter"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    startPos = QPoint();
+    endPos = QPoint();
+    inDrawing = false;
+    setMouseTracking(true);
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+//---------------------------------------------------------------------------------
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        if (!inDrawing)
+        {
+            startPos = event->pos();
+        }
+        else
+        {
+            endPos = event->pos();
+
+            QLine line = QLine(startPos, event->pos());
+            lines.append(line);
+        }
+
+        inDrawing = !inDrawing;
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (inDrawing)
+    {
+        endPos = event->pos();
+        update();
+    }
+}
+void MainWindow::drawLines(QPainter *p)
+{
+    if (!startPos.isNull() && !endPos.isNull())
+    {
+        p->drawLine(startPos, endPos);
+    }
+
+    p->drawLines(lines);
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter p(this);
+    QPen pen;
+    pen.setColor(Qt::red);
+    pen.setWidth(4);
+    p.setPen(pen);
+    drawLines(&p);
+}
---
+++ Interfaz_Canvas/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+
+private:
+    Ui::MainWindow *ui;
+    void drawLines(QPainter *p);
+
+    QPoint startPos;
+    QPoint endPos;
+    bool inDrawing;
+    QVector<QLine> lines;
+};
+
+
+#endif // MAINWINDOW_H
